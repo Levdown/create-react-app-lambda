74a5fcb348e6f892a2bcd5c0733b0dd0e46f5c4e
Лев Низковский <levniz@MacBook-Air-Lev.local>
2023-09-17 20:23:02 +0300

    test

--- src/db/db.js
+++ src/db/db.js
@@ -1,19 +1,17 @@
-import pizzaImg from "../images/pizza.png";
-import burgerImg from "../images/burger.png";
-import cocaImg from "../images/coca.png";
-import saladImg from "../images/salad.png";
-import waterImg from "../images/water.png";
-import iceCreamImg from "../images/icecream.png";
-import kebabImg from "../images/kebab.png";
+import pizzaImg from "../images/1.jpg";
+import burgerImg from "../images/2.jpg";
+import cocaImg from "../images/3.jpg";
+import saladImg from "../images/4.jpg";
+import waterImg from "../images/5.jpg";
+// import iceCreamImg from "../images/icecream.png";
+// import kebabImg from "../images/kebab.png";
 
 export function getData() {
   return [
-    { title: "Pizza", price: 17.99, Image: pizzaImg,id:1 },
-    { title: "Burger", price: 15, Image: burgerImg,id:2 },
-    { title: "Coca", price: 3.5, Image: cocaImg ,id:3},
-    { title: "Kebab", price: 13.99, Image: kebabImg,id:4 },
-    { title: "Salad", price: 2.5, Image: saladImg,id:5 },
-    { title: "Bottle of water", price: 0.99, Image: waterImg,id:6 },
-    { title: "Ice cream", price: 2.99, Image: iceCreamImg,id:7 },
+    { title: "VideoReg1", price: 17.99, Image: pizzaImg,id:1 },
+    { title: "VideoReg2", price: 15, Image: burgerImg,id:2 },
+    { title: "VideoReg3", price: 3.5, Image: cocaImg ,id:3},
+    { title: "VideoReg4", price: 2.5, Image: saladImg,id:4 },
+    { title: "VideoReg5", price: 0.99, Image: waterImg,id:6 }
   ];
 }
